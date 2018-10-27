5fa1f5a9c33a3d278cc75e8198509676874423a8
DESKTOP-826PF7U\anton <anton_eklof@hotmail.com>
2018-10-27 18:03:00 +0200

    Added customerDto A dataTransferObject. Also imported Automapper to map the customer to customerdto.

---
+++ Bookley/Controllers/Api/CustomersController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bookley.Models;
+using Bookley.Dtos;
+using AutoMapper;
+
+namespace Bookley.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {                                                              //delegate!
+            return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
+        }
+        //Get /api/customers
+        public CustomerDto GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpRequestException(HttpStatusCode.NotFound.ToString());
+            return Mapper.Map<Customer, CustomerDto>(customer);
+        }
+
+
+        //Post /api/customers
+        [HttpPost]
+        public CustomerDto CreateCustomer(CustomerDto customeDto) {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var customer = Mapper.Map<CustomerDto, Customer>(customeDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customeDto.Id = customer.Id;
+
+            return customeDto;
+        }
+
+        // Put /api/customers/1
+        [HttpPut]
+        public void UppdateCustomer(int id, CustomerDto customerDto) {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(customerDto, customerInDb);
+
+            _context.SaveChanges();
+        }
+
+        // Delete /api/customer/1
+        [HttpDelete]
+        public void DeleteCustomer(int id) {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+        }
+
+
+    }
+}
---
+++ Bookley/Dtos/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Bookley.Models;
+namespace Bookley.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]//Data anotations.
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public byte MembershipTypeId { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
